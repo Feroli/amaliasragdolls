0248e936a95c21129c8dc3b0b18ee3c2bc55a348
Fernando Ania <feroli@Fernandos-MacBook-Pro.local>
2017-11-21 13:29:38 +0000

    update: transparent nav bar on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+declare var $;
 
 @Component({
   selector: 'app-home',
@@ -20,6 +21,26 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.videoUrl = '../assets/beatbox';
     this.cat = { 'location': 'http://3.bp.blogspot.com/-cKkcZkF8BbU/VXNbmbk7mNI/AAAAAAAATDQ/dt9ffqmJPoU/s1600/otto-2.jpg', 'description': 'des...', 'title': 'ozzy' };
+
+    $('.nav-wrapper').addClass('transparent_background');
+    $('nav').addClass('z-depth-1');
+
+    $('.nav-wrapper').removeClass('grey');
+    $('.nav-wrapper').removeClass('darken-4');
+    $('nav').removeClass('z-depth-2');
+
+
+
+  }
+
+
+  ngOnDestroy() {
+    $('.nav-wrapper').addClass('grey');
+    $('.nav-wrapper').addClass('darken-4');
+    $('nav').addClass('z-depth-2');
+    $('nav').removeClass('z-depth-1');
+
+
 
   }
 
